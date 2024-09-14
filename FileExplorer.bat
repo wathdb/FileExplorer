6d45f881da66e4d671d92ab4684c2f79d177e437
wathD <114563307+wathdb@users.noreply.github.com>
2024-09-14 12:19:48 +0200

    Add files via upload

---
+++ FileExplorer.bat
@@ -0,0 +1,23 @@
+@dir
+:main
+@echo Commands list: del, cd, start, back, exit, create, move. (For more help, read the READ.ME on the GitHub)
+@set /p command=Command : 
+@IF %command%==exit (exit)
+@IF %command%==back (cd ../)
+@cls
+@dir
+@IF %command%==back (@goto main)
+@set /p file=File name : 
+@IF %command%==move (@goto move)
+@IF %command%==cd (@cd %file%)
+@IF %command%==del (@del %file%)
+@IF %command%== (@echo Veuillez entrer une commande)
+@IF %command%==start (start "" "%file%")
+@IF %command%==create (type nul > %file%)
+@cls
+@dir
+@goto main
+:move
+@set /p destination=Destination :
+@move %file% %destination%
+@goto main
